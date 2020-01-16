ace61cae0c44891f1570916c5386fa29ac2b6c73
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-16 04:38:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,6 +54,10 @@ starting_node = {
                   label: "Family",
                 sub_category: {
                 label: "Genus",
+                    sub_category: {
+                        label: "Species",
+                          sub_category: nil
+                    }
                   }
                 }
             }
